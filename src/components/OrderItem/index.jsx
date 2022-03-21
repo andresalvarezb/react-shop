5521426eebc79eb5ffccb4fdd8668a7280b93696
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-03-21 14:36:08 -0500

    feat: add logic to remove a cart

--- src/components/OrderItem/index.jsx
+++ src/components/OrderItem/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './orderItem.css'
 
 import iconClose from '../../assets/icons/icon_close.png'
+import AppContext from '../../context/AppContext';
 
 // ? stateless
 function OrderItem({product}) {
+
+    const {removeFromCart} = useContext(AppContext)
     const {images, title, price} = product;
+
+    const handleRemove = item => {
+        removeFromCart(item)
+    }
+
     return (
         <div className="OrderItem">
             <figure>
@@ -13,7 +21,7 @@ function OrderItem({product}) {
             </figure>
             <p>{title}</p>
             <p>${price}</p>
-            <img src={iconClose} alt="close" />
+            <img src={iconClose} alt="close" onClick={() => handleRemove(product)} />
         </div>
     )
 }
